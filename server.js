f81231f56b1874ee04336b6382bfb3610d9e679a
AmirKAT <amir-katal@hotmail.com>
2021-01-28 15:35:51 +0000

    setting route endpoints for lesson and order

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const express = require('express');
 const app = express();
 
 
-
-
-
-
 app.use(express.json({ limit: '50mb', extended: false }));
 
-
-
-
+app.use('/api/lesson', require('./routes/lesson'));
+app.use('/api/order', require('./routes/order'));
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => { console.log('server started on port' + PORT) });
